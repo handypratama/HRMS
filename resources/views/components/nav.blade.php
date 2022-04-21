49a69d68d60164308853b75d677891e2ec5e0560
Handy Pratama <handy.pratama@merkleinnovation.co.id>
2022-04-21 18:40:13 +0700

    Initial Commit

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-light static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="logo-merkle.png" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav justify-content-end">
+<li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#staticBackdrop" href="#form-cuti">Form Cuti</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#staticBackdrop1" href="#lihat-sisa-cuti">Lihat Sisa Cuti</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" href="#bagan-organisasi">Bagan Organisasi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#staticBackdrop3" href="#reimbursement-form">Reimbursement Form</a>
+            </li>
+</ul>
+    </div>
+  </div>
+</nav>
+<style>
+.nav-link px-4:active {
+  color:grey;
+};
+</style>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,116 @@
+@include('components.nav')
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,700%7COpen+Sans:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href=".css">
+    <title>Human Resource Management System</title>
+  </head>
+  <body>
+  <style>
+    body {
+    background-image: url('background.jpg');
+    background-position: center;
+    
+    }
+    .nav-link {
+    color: black;
+}
+</style>
+
+<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> 
+
+<div class="site-container">
+   
+            <div class="align-self-lg-center text-center">
+                <h1>Human Resource Management System</h1>
+                <p>Choose one of the menu below</p>
+                <a href="https://docs.google.com/spreadsheets/d/1qscArah2D1G_pFUgkFSZU4e72ubkyHi-bjR_QugDjNg/edit?usp=sharing" target="_blank" class="btn btn-dark btn-norounded"  >Form Cuti</a>
+                <a href="https://docs.google.com/spreadsheets/d/1wPAQI7u1tNmurXwcbE6lQms7xUDIdjtL9NTmPIX2TRI/edit?usp=sharing" target="_blank" class="btn btn-outline-dark btn-norounded">Lihat Sisa Cuti</a>
+                <a href="https://docs.google.com/spreadsheets/d/1PgSYM1mK4n--iIioOyk_ZhtsR266GNp68xP8iDOfr2g/edit?usp=sharing" target="_blank" class="btn btn-dark btn-norounded">Bagan Organisasi</a>
+                <a href="https://docs.google.com/spreadsheets/d/1gESdNii1x9AF86NRGsTQGds-QoyQkjWyxo7RnQoEROI/edit?usp=sharing" target="_blank" class="btn btn-outline-dark btn-norounded">Reimbursement Form</a>
+            </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Apa itu Form Cuti?</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <p>Mauris ut sem elementum, tristique velit sed, volutpat mauris. Ut interdum nibh sit amet imperdiet condimentum. Nulla sed tellus placerat, vehicula risus ac, sollicitudin quam. Quisque pharetra porta libero, a sagittis nisl tristique non. Etiam sapien purus, cursus eu pulvinar et, tempor sit amet ipsum. Sed a nisl sit amet lorem dictum hendrerit.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-dark btn-norounded" data-bs-dismiss="modal">Close</button>
+        <a href="https://docs.google.com/spreadsheets/d/1qscArah2D1G_pFUgkFSZU4e72ubkyHi-bjR_QugDjNg/edit?usp=sharing" target="_blank" class="btn btn-dark btn-norounded"  >View Form</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="staticBackdrop1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Lihat Sisa Cuti</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <p>Vestibulum fringilla dolor est, vitae rhoncus nisl condimentum eget. Nunc interdum facilisis velit, et condimentum nibh semper ac. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-dark btn-norounded" data-bs-dismiss="modal">Close</button>
+        <a href="https://docs.google.com/spreadsheets/d/1wPAQI7u1tNmurXwcbE6lQms7xUDIdjtL9NTmPIX2TRI/edit?usp=sharing" target="_blank" class="btn btn-dark btn-norounded">View Form</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Bagan Organisasi</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-dark btn-norounded" data-bs-dismiss="modal">Close</button>
+        <a href="https://docs.google.com/spreadsheets/d/1PgSYM1mK4n--iIioOyk_ZhtsR266GNp68xP8iDOfr2g/edit?usp=sharing" target="_blank" class="btn btn-dark btn-norounded">View Form</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="staticBackdrop3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Apa itu Reimbursement Form?</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <p>Tellus integer feugiat scelerisque varius. Aliquet enim tortor at auctor urna nunc id cursus metus. Ut tellus elementum sagittis vitae et. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Amet luctus venenatis lectus magna fringilla urna porttitor. Id aliquet risus feugiat in ante metus dictum at tempor. Dictum fusce ut placerat orci nulla pellentesque dignissim. Egestas maecenas pharetra convallis posuere morbi. Elementum facilisis leo vel fringilla est ullamcorper eget. Dolor sed viverra ipsum nunc aliquet bibendum. Arcu cursus vitae congue mauris rhoncus.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-dark btn-norounded" data-bs-dismiss="modal">Close</button>
+        <a href="https://docs.google.com/spreadsheets/d/1gESdNii1x9AF86NRGsTQGds-QoyQkjWyxo7RnQoEROI/edit?usp=sharing" target="_blank" class="btn btn-dark btn-norounded">View Form</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>                                                               
+  </body>
+</html>
